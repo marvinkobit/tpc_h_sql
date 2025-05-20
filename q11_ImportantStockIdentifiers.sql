f3a5887301e78564124a519a551921b2b58bffc7
Maeruf <maruv.nassir@gmail.com>
2025-05-20 19:00:23 +0300

    initial

---
+++ q11_ImportantStockIdentifiers.sql
@@ -0,0 +1,16 @@
+-- TPC-H Query 11: Important Stock Identifiers
+SELECT
+    ps_partkey,
+    SUM(ps_supplycost * ps_availqty) AS value
+FROM
+    partsupp,
+    supplier,
+    nation
+WHERE
+    ps_suppkey = s_suppkey
+    AND s_nationkey = n_nationkey
+    AND n_name = 'GERMANY'
+GROUP BY
+    ps_partkey
+ORDER BY
+    value DESC;
